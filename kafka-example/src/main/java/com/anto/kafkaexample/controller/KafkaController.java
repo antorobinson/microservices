62ed002de67487f2ffe75acee74eb9c8ccad6fc5
Sahaya Anto Robinson <anto_robinson@outlook.com>
2021-03-07 19:41:31 +0530

    added new project kafka

---
+++ kafka-example/src/main/java/com/anto/kafkaexample/controller/KafkaController.java
@@ -0,0 +1,26 @@
+package com.anto.kafkaexample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.anto.kafkaexample.service.Producer;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+}
